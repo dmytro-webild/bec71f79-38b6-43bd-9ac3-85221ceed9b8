fe7dd473a5884e39394aaff682f7907a22634a0b
dk <kudin.dima@gmail.com>
2026-05-10 03:53:24 +0000

    Initial commit

---
+++ src/app/layout.tsx
@@ -0,0 +1,48 @@
+import type { Metadata } from "next";
+import { Halant } from "next/font/google";
+import { Inter } from "next/font/google";
+import "./globals.css";
+import "@/lib/gsap-setup";
+import { ServiceWrapper } from "@/components/ServiceWrapper";
+import Tag from "@/tag/Tag";
+import { getVisualEditScript } from "@/utils/visual-edit-script";
+import { Lato } from "next/font/google";
+
+
+
+export const metadata: Metadata = {
+  title: 'Sawgrass Mills - South Florida Shopping & Dining',
+  description: 'Experience South Florida\'s world-renowned shopping destination with premier luxury outlets and dining.',
+  openGraph: {
+    "siteName": "Sawgrass Mills",
+    "type": "website"
+  },
+};
+
+const lato = Lato({
+  variable: "--font-lato",
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "700", "900"],
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <ServiceWrapper>
+        <body className={`${lato.variable} antialiased`}>
+          <Tag />
+          {children}
+          <script
+              dangerouslySetInnerHTML={{
+                  __html: `${getVisualEditScript()}`
+              }}
+          />
+        </body>
+      </ServiceWrapper>
+    </html>
+  );
+}
